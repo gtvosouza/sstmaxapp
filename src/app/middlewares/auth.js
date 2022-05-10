18a09d4862a9edf066d63df3905f82773bdd648e
unknown <gtvosouza@hotmail.com>
2022-05-10 19:10:18 -0300

    no token

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -3,7 +3,7 @@ const authConfig = require("../../config/auth.json");
 
 module.exports = (req, res, next) => {
     const authHeader = req.headers.authorization;
-
+    /*
     if (!authHeader)
         return res.status(401).send({ error: 'No token provided'});
 
@@ -16,6 +16,8 @@ module.exports = (req, res, next) => {
 
     if (!/^Bearer$/i.test(scheme))
         return res.status(401).send({ error: 'Token malformatted'});
+        */
+
 
     jwt.verify(token, authConfig.secret, (err, decoded) => {
         if (err)  return res.status(401).send({ error: 'Token invalid'});
